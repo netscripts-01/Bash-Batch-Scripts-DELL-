9b2fccd0558113a84759049c7a79ab807b36ca9c
netscripts-01 <57531656+netscripts-01@users.noreply.github.com>
2020-06-04 12:33:17 +0200

    Update batch_create_snmp_community.sh

--- Bash_Scripts_Dell/SNMP/batch_create_snmp_community.sh
+++ Bash_Scripts_Dell/SNMP/batch_create_snmp_community.sh
@@ -5,6 +5,6 @@ cd ~/Documents/Scripts/SNMP/BatchCreateDelete_SNMP
 
 cat ips.txt | while read "ip"
 do
-    expect -f ~/Documents/Scripts/SNMP/BatchCreateDelete_SNMP/create_snmp_user_proleit.expect $ip
+    expect -f ~/Documents/Scripts/SNMP/BatchCreateDelete_SNMP/create_snmp_user.expect $ip
 
 done
